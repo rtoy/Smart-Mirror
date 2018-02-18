53573d315834d664b1b9605da13b2e7277cbaeb4
Raymond Toy <toy.raymond@gmail.com>
2018-02-18 10:36:28 -0800

    Use somewhat smaller font sizes
    The default sizes are too big for my mirror.

--- smartmirror.py
+++ smartmirror.py
@@ -25,10 +25,10 @@ weather_lang = 'en' # see https://darksky.net/dev/docs/forecast for full list of
 weather_unit = 'us' # see https://darksky.net/dev/docs/forecast for full list of unit parameters values
 latitude = None # Set this if IP location lookup does not work for you (must be a string)
 longitude = None # Set this if IP location lookup does not work for you (must be a string)
-xlarge_text_size = 94
-large_text_size = 48
-medium_text_size = 28
-small_text_size = 18
+xlarge_text_size = 64 #94
+large_text_size = 36 #48
+medium_text_size = 24 #28
+small_text_size = 12 #18
 
 @contextmanager
 def setlocale(name): #thread proof function to work with locale
